38f5d007d603582f4d0d1607bfc2c71b23276364
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-07-20 08:01:00 +0900

    make stop when block

--- src/player/index.ts
+++ src/player/index.ts
@@ -17,9 +17,10 @@ export default class SetModel {
     private _scene:THREE.Scene;
 
     private counter = 0;
-
-
     private checker:Checker
+
+    private previousLocation:THREE.Vector3 = new THREE.Vector3()
+
     constructor(
         scene: THREE.Scene, 
         camera: THREE.Camera, 
@@ -144,18 +145,49 @@ export default class SetModel {
             this._camera.position.y -= 0.1
         }
 
-        console.log(this._camera.position)
+        // collusion
+        // x좌표, -x, +z,-z
+        // 로 가면 막힘
+
+        // console.log(this._camsera.position)
+
+        // 아래 if 문에 들어갔을 때 state를 false해서 값을 저장하지 않고,
+        // 안 들어가면 전의 위치 저장,
+        // if문에 들어가면 저장된 위치로 카메라 이동
+        // 왜인진 모르겠는데 안 됐음
+
+        // let flag = true;
 
         // moveForward이기 때문에 x,z 는 비교해서 처리
+        if (!collusion[0]){
+            this._camera.position.x -= speed
+            // this._camera.position.x = this.previousLocation.x
+            // flag = false
+        } 
         if (!collusion[1]){
-            this._camera.position
+            this._camera.position.x += speed
+            // this._camera.position.x = this.previousLocation.x
+            // flag = false
         }
-
-
+        if (!collusion[2]){
+            this._camera.position.z -= speed
+            // this._camera.position.z = this.previousLocation.z
+            // flag = false
+        }
+        if (!collusion[3]){
+            this._camera.position.z += speed
+            // this._camera.position.z = this.previousLocation.z
+            // flag = false
+        }
+        
+        
         this._model.position.x = this._camera.position.x
         this._model.position.y = this._camera.position.y - 1
         this._model.position.z = this._camera.position.z;
-
         
+        // if (flag){
+        //     this.previousLocation = this._camera.position
+        //     console.log("save",this.previousLocation)
+        // }
     }
 }

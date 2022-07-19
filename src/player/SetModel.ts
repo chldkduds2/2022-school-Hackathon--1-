9adf045808d86b6d6923b6ce99a848169019fbdb
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-07-19 16:12:48 +0900

    make controller

--- src/player/SetModel.ts
+++ src/player/SetModel.ts
@@ -10,6 +10,9 @@ export default class SetModel{
     private _modal:HTMLElement
     private pointerLockControl
 
+    private _state = [false,false,false,false,false,false]
+
+
     constructor(scene:THREE.Scene,camera:THREE.Camera,divContainer:HTMLElement){
         this._camera = camera;
         this._divContainer=divContainer
@@ -32,5 +35,61 @@ export default class SetModel{
             scene.add(gltf.scene)
         },u => console.log(u), e => console.log(e))
 
+
+        window.addEventListener("keydown",(e) => {
+            this.changeState(e.keyCode)
+        })
+        
+        window.addEventListener("keyup",(e) => {
+            this.changeState(e.keyCode)
+        })
+
+        
     }
+
+
+    changeState(keyCode:number){
+        if (keyCode == 119 || keyCode == 87 ){ // w
+            this._state[0] = !(this._state[0])
+        } 
+        if (keyCode == 65 || keyCode == 97){ // a
+            this._state[1] = !(this._state[1])
+        }
+        if (keyCode == 68 || keyCode == 100){// d
+            this._state[2] = !(this._state[2])
+        }
+        if (keyCode == 83 || keyCode == 115){//s
+            this._state[3] = !(this._state[3])
+        }
+        if (keyCode == 32){
+            this._state[4] = !(this._state[4]) // space
+        } 
+        if (keyCode == 16){ // shitf
+            this._state[5] = !(this._state[5])
+        }
+    }
+
+
+    update(){
+        const state = this._state;
+        if (state[0]){
+            this.pointerLockControl.moveForward(1)
+        }
+        if (state[1]){
+            this.pointerLockControl.moveRight(-1)
+        }
+        if (state[2]){
+            this.pointerLockControl.moveRight(1)
+        }
+        if (state[3]){
+            this.pointerLockControl.moveForward(-1)
+        }
+        if (state[4]){
+            this._camera.position.y += 1
+        }
+        if (state[5]){
+            this._camera.position.y -= 1
+        }
+    }
+
 }

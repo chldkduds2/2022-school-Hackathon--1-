6173c69ff5047752646702aa89d512937147c83b
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-07-20 08:49:50 +0900

    fix checker

--- src/player/Checker.ts
+++ src/player/Checker.ts
@@ -10,14 +10,18 @@ export default class Checker {
         // console.log(this.world.worldInfo.palette)
     }
     public update(pos:THREE.Vector3):Array<boolean> {
-        pos = new THREE.Vector3(Math.floor(pos.x), Math.floor(pos.z), Math.floor(pos.y))
+        pos = new THREE.Vector3(pos.x, pos.z, Math.floor(pos.y))
+        // pos = new THREE.Vector3(Math.floor(pos.x), Math.floor(pos.z), Math.floor(pos.y))
+
+        console.log(pos)
+
         return [
-            this.possable(this.world.map.get(new Vector3(Math.floor(pos.x + 1), Math.floor(pos.y), Math.floor(pos.z-1)))),
-            this.possable(this.world.map.get(new Vector3(pos.x - 1, pos.y, pos.z-1))),
-            this.possable(this.world.map.get(new Vector3(pos.x, pos.y + 1, pos.z-1))),
-            this.possable(this.world.map.get(new Vector3(pos.x, pos.y - 1, pos.z-1))),
-            this.possable(this.world.map.get(new Vector3(pos.x, pos.y, pos.z + 1))),
-            this.possable(this.world.map.get(new Vector3(pos.x, pos.y, pos.z - 2)))
+            this.possable(this.world.map.get(new Vector3(Math.floor(pos.x+0.2), Math.floor(pos.y), pos.z-1))),
+            this.possable(this.world.map.get(new Vector3(Math.floor(pos.x-0.2), Math.floor(pos.y), pos.z-1))),
+            this.possable(this.world.map.get(new Vector3(Math.floor(pos.x), Math.floor(pos.y + 0.2), pos.z-1))),
+            this.possable(this.world.map.get(new Vector3(Math.floor(pos.x), Math.floor(pos.y - 0.2), pos.z-1))),
+            this.possable(this.world.map.get(new Vector3(Math.floor(pos.x), Math.floor(pos.y), pos.z + 1))),
+            this.possable(this.world.map.get(new Vector3(Math.floor(pos.x), Math.floor(pos.y), pos.z - 2)))
         ]
     }
     private possable(code:number) {
